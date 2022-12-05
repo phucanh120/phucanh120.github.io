811f61907dc9544ab1089f7389c6d8c9260bcda0
phucanh120 <118629088+phucanh120@users.noreply.github.com>
2022-12-05 20:15:46 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,63 @@
-window.onload = (e) => {
-    var audio_element = document.createElement('audio');
-
-    var source_in_audio = document.createElement('source');
-
-    var source_element = document.getElementById('music_source');
-
-    audio_element.setAttribute('controls', 'controls');
-
-    audio_element.style.opacity = 0;
-
-    audio_element.style.position = 'fixed';
-
-    audio_element.style.top = 0;
-
-    audio_element.style.pointerEvents = 'none';
-
-    audio_element.id = 'music_file';
-
-    source_in_audio.id = 'music_file_src';
-
-    source_in_audio.setAttribute('src', list_music[0].src);
-
-    source_in_audio.setAttribute('type', 'audio/mp3');
-
-    document.getElementById('music_name').innerHTML = list_music[0].name;
-
-    document.getElementById('author').innerHTML = list_music[0].author;
-
-    document.getElementById('music_image').style.setProperty("--music-img", 'url(\'' + list_music[0].img + '\')');
-
-    document.getElementById('audio_playing').setAttribute('audio', '0');
-
-    audio_element.append(source_in_audio);
-
-    source_element.append(audio_element);
-}
-
 const list_music = [
     {
-        'name': 'Chỉ cần ai đó cạnh bên dừng lại',
-        'author': 'Nhạc Hot Tiktok',
-        'src': './src/music/y2mate.com - 3107 2 Remix  Chỉ cần ai đó cạnh bên dừng lại  Nhạc Hot Tiktok hay nhất 2022.mp3',
-        'img': '../img/music/ff1.jpg'
+        'name': "lucky",
+        'author': 'phucanh',
+        'src': './src/music/y2mate.com - LUCKY TWICE REMIX Vinz x Nam Milano Exclusive Nhạc Hot TikTok Hay Nhất 2022 TannHee Music.mp3',
+        'img': '../img/music/f11.jpg'
     },
     {
-        'name': 'Vương Vấn x Chưa Chắc Đã Giống Đâu',
-        'author': 'Beat Remix',
-        'src': './src/music/y2mate.com - Vương Vấn x Chưa Chắc Đã Giống Đâu Beat Remix  Nhạc Hot Tik Tok 2022.mp3',
-        'img': '../img/music/tải xuống.jpg'
+        'name': "some thing",
+        'author': "phucanh",
+        'src': './src/music/y2mate.com - The Chainsmokers Coldplay Something Just Like This Asher Remix Cover.mp3',
+        'img': '../img/music/f11.jpg'
     },
     {
-        'name': "Something Just Like ",
-        'author': 'Asher',
-        'src': './src/music/y2mate.com - The Chainsmokers  Coldplay  Something Just Like This Asher Remix Cover.mp3',
-        'img': '../img/music/a26bd3b88f30608733ab7cdc630066e6.jpg'
-    }
+        'name': "thái bình mồ hôi rơi",
+        'author': "phucanh",
+        'src': './src/music/y2mate.com - Thái Bình Mồ Hôi Rơi Remix Vinz Mix Nhạc Hot Tik Tok.mp3',
+        'img': '../img/music/f11.jpg'
+    },
+    {
+        'name': "3107",
+        'author': 'phucanh',
+        'src': './src/music/y2mate.com - 3107 2 Remix Chỉ cần ai đó cạnh bên dừng lại Nhạc Hot Tiktok hay nhất 2022.mp3',
+        'img': '../img/music/f11.jpg'
+    },
+    {
+        'name': "HEATHENS REMIX",
+        'author': 'phucanh',
+        'src': './src/music/y2mate.com - HEATHENS REMIX TIKTOK NHẠC CHIẾN ĐÉT HOT TREND TIKTOK.mp3',
+        'img': '../img/music/f11.jpg'
+    },
+    {
+        'name': "vẫn vương",
+        'author': 'phucanh',
+        'src': './src/music/y2mate.com - Vương Vấn x Chưa Chắc Đã Giống Đâu Beat Remix Nhạc Hot Tik Tok 2022.mp3',
+        'img': '../img/music/f11.jpg'
+    },
+    {
+        'name': "lovely dog remix",
+        'author': 'phucanh',
+        'src': './src/music/y2mate.com - Lovely Dog Remix TikTok ARS Remix Nhạc Toàn Mùi MIKENCO MIKENCO Trend Remix.mp3',
+        'img': '../img/music/f11.jpg'
+    },
+    {
+        'name': "Trang Trại Cần Sa",
+        'author': "phucanh",
+        'src': './src/music/y2mate.com - 3 IN 1 X FEEL TRANG TRẠI CẦN REMIX NHẠC HOT TIK TOK 2022.mp3',
+        'img': '../img/music/f11.jpg'
+    },
 ];
+
+window.onload = (e) => {
+    audio_action(0, true);
+}
 
 const soundOnChange = (e) => {
     const el = e;
     el.style.setProperty("--value", el.value);
     el.style.setProperty("--min", el.min === "" ? "0" : el.min);
     el.style.setProperty("--max", el.max === "" ? "100" : el.max);
-    el.style.setProperty("--value", el.value);
 
     var volume = el.value;
 
@@ -70,6 +65,18 @@ const soundOnChange = (e) => {
 
     audio.volume = volume;
 };
+
+const timelineOnChange = (e) => {
+    const el = e;
+    el.style.setProperty("--value", el.value);
+    el.style.setProperty("--min", el.min === "" ? "0" : el.min);
+
+    var time = el.value;
+
+    var audio = document.getElementById('music_file');
+
+    audio.currentTime = time;
+}
 
 const createAlert = (message) => {
     var alert_ = document.createElement('div');
@@ -94,8 +101,6 @@ const createAlert = (message) => {
 };
 
 const audio_is_end = () => {
-    var play_button = document.getElementById('play');
-
     var current_audio = document.getElementById('audio_playing');
 
     var get_current_audio = parseInt(current_audio.getAttribute('audio'));
@@ -103,99 +108,9 @@ const audio_is_end = () => {
     var count_audio = list_music.length;
 
     if (get_current_audio >= count_audio - 1) {
-        document.getElementById('music_file').remove();
-
-        var audio_element = document.createElement('audio');
-    
-        var source_in_audio = document.createElement('source');
-    
-        var source_element = document.getElementById('music_source');
-    
-        var volume_setting = document.getElementById('volume_setting');
-    
-        audio_element.setAttribute('controls', 'controls');
-    
-        audio_element.style.opacity = 0;
-    
-        audio_element.style.position = 'fixed';
-    
-        audio_element.style.top = 0;
-    
-        audio_element.style.pointerEvents = 'none';
-    
-        audio_element.id = 'music_file';
-    
-        source_in_audio.id = 'music_file_src';
-    
-        source_in_audio.setAttribute('src', list_music[0].src);
-    
-        source_in_audio.setAttribute('type', 'audio/mp3');
-    
-        document.getElementById('music_name').innerHTML = list_music[0].name;
-    
-        document.getElementById('author').innerHTML = list_music[0].author;
-    
-        document.getElementById('music_image').style.setProperty("--music-img", 'url(\'' + list_music[0].img + '\')');
-    
-        document.getElementById('audio_playing').setAttribute('audio', '0');
-    
-        audio_element.append(source_in_audio);
-    
-        source_element.append(audio_element);
-    
-        audio_element.volume = volume_setting.value;
-
-        audio_element.addEventListener('ended', audio_is_end);
-    
-        play_button.innerHTML = '<i class="fas fa-play"></i>';
+        audio_action(0);
     } else {
-        document.getElementById('music_file').remove();
-
-        var audio_element = document.createElement('audio');
-    
-        var source_in_audio = document.createElement('source');
-    
-        var source_element = document.getElementById('music_source');
-    
-        var volume_setting = document.getElementById('volume_setting');
-    
-        audio_element.setAttribute('controls', 'controls');
-    
-        audio_element.style.opacity = 0;
-    
-        audio_element.style.position = 'fixed';
-    
-        audio_element.style.top = 0;
-    
-        audio_element.style.pointerEvents = 'none';
-    
-        audio_element.id = 'music_file';
-    
-        source_in_audio.id = 'music_file_src';
-    
-        source_in_audio.setAttribute('src', list_music[get_current_audio + 1].src);
-    
-        source_in_audio.setAttribute('type', 'audio/mp3');
-    
-        document.getElementById('music_name').innerHTML = list_music[get_current_audio + 1].name;
-    
-        document.getElementById('author').innerHTML = list_music[get_current_audio + 1].author;
-    
-        document.getElementById('music_image').style.setProperty("--music-img", 'url(\'' + list_music[get_current_audio + 1].img + '\')');
-    
-        document.getElementById('audio_playing').setAttribute('audio', get_current_audio + 1);
-    
-        audio_element.append(source_in_audio);
-    
-        source_element.append(audio_element);
-    
-        audio_element.volume = volume_setting.value;
-    
-        audio_element.play();
-
-        audio_element.addEventListener('ended', audio_is_end);
-    
-        play_button.innerHTML = '<i class="fas fa-pause"></i>';
+        audio_action(get_current_audio + 1);
     }
 }
 
@@ -211,12 +126,44 @@ const play_audio = () => {
         audio.play();
         play_button.innerHTML = '<i class="fas fa-pause"></i>';
 
+        if (document.getElementById('if_first_play').innerHTML == '1') {
+            document.getElementById('timeline_setting').disabled = false;
+            document.getElementById('if_first_play').innerHTML = '0';
+        }
+
+        const el = document.getElementById('timeline_setting');
+
+        var set = setInterval(function () {
+            if (document.getElementById('timer').innerHTML == 'broken') {
+                document.getElementById('timer').innerHTML = '';
+
+                clearInterval(set);
+            }
+
+            if (audio.currentTime >= audio.duration) {
+                clearInterval(set);
+            }
+
+            audio = document.getElementById('music_file');
+
+            el.setAttribute('value', audio.currentTime);
+            el.setAttribute('max', audio.duration);
+            el.style.setProperty("--value", audio.currentTime);
+            el.style.setProperty("--min", 0);
+            el.style.setProperty("--max", audio.duration);
+
+        }, 1);
+
         audio.addEventListener('ended', audio_is_end);
     }
 }
 
 const next_audio = () => {
-    var play_button = document.getElementById('play');
+    var if_first_play = document.getElementById('if_first_play').innerHTML;
+
+    if (if_first_play == '0') {
+        document.getElementById('timer').innerHTML = 'broken';
+    }
 
     var current_audio = document.getElementById('audio_playing');
 
@@ -227,58 +174,16 @@ const next_audio = () => {
     if (get_current_audio >= count_audio - 1) {
         createAlert('Bản nhạc hiện tại là cuối cùng.');
     } else {
-        document.getElementById('music_file').remove();
-
-        var audio_element = document.createElement('audio');
-    
-        var source_in_audio = document.createElement('source');
-    
-        var source_element = document.getElementById('music_source');
-    
-        var volume_setting = document.getElementById('volume_setting');
-    
-        audio_element.setAttribute('controls', 'controls');
-    
-        audio_element.style.opacity = 0;
-    
-        audio_element.style.position = 'fixed';
-    
-        audio_element.style.top = 0;
-    
-        audio_element.style.pointerEvents = 'none';
-    
-        audio_element.id = 'music_file';
-    
-        source_in_audio.id = 'music_file_src';
-    
-        source_in_audio.setAttribute('src', list_music[get_current_audio + 1].src);
-    
-        source_in_audio.setAttribute('type', 'audio/mp3');
-    
-        document.getElementById('music_name').innerHTML = list_music[get_current_audio + 1].name;
-    
-        document.getElementById('author').innerHTML = list_music[get_current_audio + 1].author;
-    
-        document.getElementById('music_image').style.setProperty("--music-img", 'url(\'' + list_music[get_current_audio + 1].img + '\')');
-    
-        document.getElementById('audio_playing').setAttribute('audio', get_current_audio + 1);
-    
-        audio_element.append(source_in_audio);
-    
-        source_element.append(audio_element);
-    
-        audio_element.volume = volume_setting.value;
-    
-        audio_element.play();
-
-        audio_element.addEventListener('ended', audio_is_end);
-    
-        play_button.innerHTML = '<i class="fas fa-pause"></i>';
+        audio_action(get_current_audio + 1);
     }
 }
 
 const previous_audio = () => {
-    var play_button = document.getElementById('play');
+    var if_first_play = document.getElementById('if_first_play').innerHTML;
+
+    if (if_first_play == '0') {
+        document.getElementById('timer').innerHTML = 'broken';
+    }
 
     var current_audio = document.getElementById('audio_playing');
 
@@ -287,53 +192,7 @@ const previous_audio = () => {
     if (get_current_audio == 0) {
         createAlert('Bản nhạc hiện tại là đầu tiên.');
     } else {
-        document.getElementById('music_file').remove();
-
-        var audio_element = document.createElement('audio');
-    
-        var source_in_audio = document.createElement('source');
-    
-        var source_element = document.getElementById('music_source');
-    
-        var volume_setting = document.getElementById('volume_setting');
-    
-        audio_element.setAttribute('controls', 'controls');
-    
-        audio_element.style.opacity = 0;
-    
-        audio_element.style.position = 'fixed';
-    
-        audio_element.style.top = 0;
-    
-        audio_element.style.pointerEvents = 'none';
-    
-        audio_element.id = 'music_file';
-    
-        source_in_audio.id = 'music_file_src';
-    
-        source_in_audio.setAttribute('src', list_music[get_current_audio - 1].src);
-    
-        source_in_audio.setAttribute('type', 'audio/mp3');
-    
-        document.getElementById('music_name').innerHTML = list_music[get_current_audio - 1].name;
-    
-        document.getElementById('author').innerHTML = list_music[get_current_audio - 1].author;
-    
-        document.getElementById('music_image').style.setProperty("--music-img", 'url(\'' + list_music[get_current_audio - 1].img + '\')');
-    
-        document.getElementById('audio_playing').setAttribute('audio', get_current_audio - 1);
-    
-        audio_element.append(source_in_audio);
-    
-        source_element.append(audio_element);
-    
-        audio_element.volume = volume_setting.value;
-    
-        audio_element.play();
-
-        audio_element.addEventListener('ended', audio_is_end);
-    
-        play_button.innerHTML = '<i class="fas fa-pause"></i>';
+        audio_action(get_current_audio - 1);
     }
 }
 
@@ -342,3 +201,121 @@ document.getElementById('play').addEventListener('click', play_audio);
 document.getElementById('next').addEventListener('click', next_audio);
 
 document.getElementById('previous').addEventListener('click', previous_audio);
+
+var list_html = ``;
+
+for (var i = 1; i <= list_music.length; i++) {
+    list_html += `
+    <div class="music_box" music="${i - 1}">
+        <div class="img" style="--img:url('${list_music[i - 1]['img']}');"></div>
+        <div>
+            <div class="name">${list_music[i - 1]['name']}</div>
+            <div class="author">by <strong>${list_music[i - 1]['author']}</strong></div>
+        </div>
+    </div>
+    `;
+
+    document.getElementById('playlist').innerHTML = `
+        ${list_html}
+    `;
+}
+
+var list_music_in_playlist = document.getElementsByClassName('music_box');
+
+for (var i = 0; i < list_music_in_playlist.length; i++) {
+
+    list_music_in_playlist[i].addEventListener('click', function () {
+
+        var current_audio = document.getElementById('audio_playing');
+
+        var get_current_audio = parseInt(current_audio.getAttribute('audio'));
+
+        var music_choose = parseInt(this.getAttribute('music'));
+
+        if (music_choose == get_current_audio) {
+            createAlert('Bản nhạc này đang được phát !');
+        } else {
+            var if_first_play = document.getElementById('if_first_play').innerHTML;
+
+            if (if_first_play == '0') {
+                document.getElementById('timer').innerHTML = 'broken';
+            }
+
+            audio_action(music_choose);
+        }
+    });
+}
+
+function audio_action(what_to_play, if_first_play) {
+    var play_button = document.getElementById('play');
+
+    if (!if_first_play) {
+        document.getElementById('music_file').remove();
+    }
+
+    var audio_element = document.createElement('audio');
+        
+    var source_in_audio = document.createElement('source');
+        
+    var source_element = document.getElementById('music_source');
+        
+    var volume_setting = document.getElementById('volume_setting');
+        
+    audio_element.setAttribute('controls', 'controls');
+        
+    audio_element.style.opacity = 0;
+        
+    audio_element.style.position = 'fixed';
+        
+    audio_element.style.top = 0;
+        
+    audio_element.style.pointerEvents = 'none';
+        
+    audio_element.id = 'music_file';
+        
+    source_in_audio.id = 'music_file_src';
+        
+    source_in_audio.setAttribute('src', list_music[what_to_play].src);
+        
+    source_in_audio.setAttribute('type', 'audio/mp3');
+        
+    document.getElementById('music_name').innerHTML = list_music[what_to_play].name;
+        
+    document.getElementById('author').innerHTML = list_music[what_to_play].author;
+        
+    document.getElementById('music_image').style.setProperty("--music-img", 'url(\'' + list_music[what_to_play].img + '\')');
+        
+    document.getElementById('audio_playing').setAttribute('audio', what_to_play);
+        
+    audio_element.append(source_in_audio);
+        
+    source_element.append(audio_element);
+
+    audio_element.volume = volume_setting.value;
+        
+    if (!if_first_play) {
+        play_audio();
+    } else {
+        document.getElementById('timeline_setting').disabled = true;
+
+        document.getElementById('if_first_play').innerHTML = '1';
+    }
+
+    audio_element.addEventListener('ended', audio_is_end);
+
+    if (!if_first_play) {
+        play_button.innerHTML = '<i class="fas fa-pause"></i>';
+
+        var list_playlist = document.querySelectorAll('[music]');
+
+        for (var i = 0; i < list_playlist.length; i++) {
+            list_playlist[i].classList.remove('is_playing');
+        }
+    } else {
+        play_button.innerHTML = '<i class="fas fa-play"></i>';
+    }
+
+    document.querySelector(`[music="${what_to_play}"]`).classList.add('is_playing');
+
+    return true;
+}
